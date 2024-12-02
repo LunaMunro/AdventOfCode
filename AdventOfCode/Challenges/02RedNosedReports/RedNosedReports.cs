d5d4bf5f51fb4a8a25177339b9e3baac59331659
LunaMunro <79856322+LunaMunro@users.noreply.github.com>
2024-12-02 15:51:11 +0000

    Added day 2 challenge

---
+++ AdventOfCode/Challenges/02RedNosedReports/RedNosedReports.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace AdventOfCode.Challenges.RedNosedReports
+{
+    internal class RedNosedReports : ChallengeBase
+    {
+        private IEnumerable<IEnumerable<int>> Data;
+
+        private RedNosedReports() { } // Private constructor to enforce factory method usage
+
+        public static async Task<RedNosedReports> CreateChallenge(string url)
+        {
+            RedNosedReports task = new RedNosedReports();
+            string taskData = await task.GetInputStringFromUrl(url);
+            task.ExtractInput(taskData);
+            return task;
+        }
+
+        public override int ProcessPart1()
+        {
+            return this.Data.Where(r => this.IsSafe(r.ToList())).Count();
+        }
+
+        public override int ProcessPart2()
+        {
+            return this.Data.Where(r => this.IsSafeWithDampener(r.ToList())).Count();
+        }
+
+        private bool IsSafe(IEnumerable<int> report)
+        {
+            var differences = report.Zip(report.Skip(1), (a, b) => a - b);
+            return differences.All(d => d >= 1 && d <= 3) || differences.All(d => d <= -1 && d >= -3);
+        }
+
+        private bool IsSafeWithDampener(List<int> report)
+        {
+            return Enumerable.Range(0, report.Count()).
+                Any(i => this.IsSafe(report.Where((num, index) => index != i)));
+        }
+
+        public override void ExtractInput(string input)
+        {
+            var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var data = new List<IEnumerable<int>>();
+
+            foreach (var line in lines)
+            {
+                var numbers = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(int.Parse);
+
+                data.Add(numbers);
+            }
+            this.Data = data;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,8 @@
 ﻿using AdventOfCode;
 using AdventOfCode.Challenges.HistorianHysteria;
+using AdventOfCode.Challenges.RedNosedReports;
 
-ChallengeBase task = await HistorianHysteria.CreateChallenge("https://adventofcode.com/2024/day/1/input");
+ChallengeBase task = await RedNosedReports.CreateChallenge("https://adventofcode.com/2024/day/2/input");
 var answer = task.ProcessPart1();
 Console.WriteLine(answer);
 
